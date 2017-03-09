923b66626701d14a5ae4e19c0f23413429cccec5
DieterMetzler <d.metzler@summsumm.com>
2017-03-09 21:08:14 +0100

    Kalibrierung der Stockwaage via SMS

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -27,17 +27,57 @@ DHT dht_pinR05(DHTPIN_R05, DHTTYPE_R05,20);
 //HX711 scale(A1, A0);      //parameter "gain" is ommited; the default value 128 is used by the library
 HX711 scale(A02, A01);
 
-float offset = 299.7;
-float scalefactor = 28075.5;
+float offset = 0;
+float scalefactor = 1;
+
+String str_scalefactor = "";
+String str_offset = "";
+
 float floatGewicht = 0;
 char charGewicht[10];
 String stringGewicht = "";
 
+void cloud_sms(const String &sender, const rtc_datetime_t &timestamp, const String &message) {
+  Serial.println("CLOUD SMS RECEIVED:");
+  Serial.print("SMS SENDER: ");
+  Serial.println(sender);
+  Serial.print("SMS TIMESTAMP: ");
+  Serial.println(timestamp);
+  Serial.println("SMS TEXT: ");
+  Serial.println(message);
+
+  int firstCommaIndex = message.indexOf(',');
+  int secondCommaIndex = message.indexOf(',', firstCommaIndex+1);
+  
+  str_scalefactor = message.substring(0, firstCommaIndex);
+  str_offset = message.substring(firstCommaIndex+1, secondCommaIndex);
+  scalefactor = str_scalefactor.toFloat(); 
+  offset = str_offset.toFloat();
+
+  Serial.println("Scalefactor: ");
+  Serial.println(scalefactor);
+  Serial.println("Offset: ");
+  Serial.println(offset);
+
+  if(HologramCloud.sendMessage(sender, "SMSRX")) {
+    Serial.println("SMS received message sent to cloud.");
+  } else {
+    Serial.println("Notification send failed.");
+    Serial.println("Check failure reason by typing:");
+    Serial.println("cloud status");
+  }
+}
+
 void setup() {
 
   pinMode(D09, OUTPUT);
-
   Dash.onLED();
+  digitalWrite(D09, HIGH);
+
+  Serial.begin();                                       //Start USB Serial
+  HologramCloud.attachHandlerSMS(cloud_sms);            //Handle received SMS
+
+  
   // put your setup code here, to run once:
   Dash.begin();
 
@@ -45,6 +85,23 @@ void setup() {
   dht_pinR04.begin();
   dht_pinR05.begin();
   
+  
+
+}
+
+void loop() {
+  
+  
+  Dash.pulseLED(1000, 1000);
+  digitalWrite(D09, HIGH);
+  delay(5000);
+
+  /*Serial.println("Scalefactor: ");
+  Serial.println(scalefactor);
+  Serial.println("Offset: ");
+  Serial.println(offset);
+*/
+
   scale.set_scale(scalefactor);                      //this value is obtained by calibrating the scale with known weights; 
                                                  /*   How to Calibrate Your Scale
                                                       1.Call set_scale() with no parameter.
@@ -53,20 +110,13 @@ void setup() {
                                                       4.Divide the result in step 3 to your known weight. You should get about the parameter you need to pass to set_scale.
                                                       5.Adjust the parameter in step 4 until you get an accurate reading. 
                                                   */
-
-}
-
-void loop() {
-
-  digitalWrite(D09, HIGH);
-  delay(5000);
    
   scale.power_up();
   HologramCloud.sendMessage(dhtJSON()); // Send JSON DHT data to Hologram Cloud  
   scale.power_down();
   HologramCloud.disconnect();
   digitalWrite(D09, LOW);
-  Dash.deepSleepMin(60);
+  Dash.deepSleepMin(15);
   
 }
 
