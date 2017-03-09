1ea898fbb5f10845409e3cf6f1df7412db640e81
DieterMetzler <d.metzler@summsumm.com>
2017-03-09 16:59:47 +0100

    Code für 2 DHT22 Sensoren

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,19 +1,28 @@
 #include "HX711.h"
-
 #include <DHT.h>
-#define Serial SerialUSB
-#define DHTPIN R04     // DHT Sensor output pin connected to Dash GPIO
-#define DHTTYPE DHT22   // DHT 22
+
+// DHT humidity/temperature sensors
+#define DHTPIN_R04 D02     // DHT Sensor output pin connected to Dash GPIO
+#define DHTPIN_R05 D03
+
+#define DHTTYPE_R04 DHT22   // DHT 22 (AM2302)
+#define DHTTYPE_R05 DHT22   // DHT 22 (AM2302)
 //Change this to change how often the DHT22 data is printed and sent to the cloud
 
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
-DHT dht(DHTPIN, DHTTYPE);
 
-//HX711.DOUT    - pin #A1
-//HX711.PD_SCK  - pin #A0
+// Initialize DHT sensor for normal 16mhz Arduino
+DHT dht_pinR04(DHTPIN_R04, DHTTYPE_R04,20);
+DHT dht_pinR05(DHTPIN_R05, DHTTYPE_R05,20);
+// NOTE: For working with a faster chip, like an Arduino Due or Teensy, you
+// might need to increase the threshold for cycle counts considered a 1 or 0.
+// You can do this by passing a 3rd parameter for this threshold.  It's a bit
+// of fiddling to find the right value, but in general the faster the CPU the
+// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+// Arduino Due that runs at 84mhz a value of 30 works.
+// Example to initialize DHT sensor for Arduino Due:
+//DHT dht(DHTPIN, DHTTYPE, 30);
+
+
 
 //HX711 scale(A1, A0);      //parameter "gain" is ommited; the default value 128 is used by the library
 HX711 scale(A02, A01);
@@ -25,9 +34,17 @@ char charGewicht[10];
 String stringGewicht = "";
 
 void setup() {
+
+  pinMode(D09, OUTPUT);
+
+  Dash.onLED();
   // put your setup code here, to run once:
-  Dash.begin();  
-  SerialUSB.begin(9600);  
+  Dash.begin();
+
+  //Begin DHT communication
+  dht_pinR04.begin();
+  dht_pinR05.begin();
+  
   scale.set_scale(scalefactor);                      //this value is obtained by calibrating the scale with known weights; 
                                                  /*   How to Calibrate Your Scale
                                                       1.Call set_scale() with no parameter.
@@ -40,60 +57,42 @@ void setup() {
 }
 
 void loop() {
-  
-  SerialCloud.begin(115200); 
-  //SerialUSB.println("Waiting for GSM network");
-  //delay(30000); //Delay to make connection to GSM network
-  Dash.snooze(30000);
- 
+
+  digitalWrite(D09, HIGH);
+  delay(5000);
+   
   scale.power_up();
-  //SerialUSB.print("Average weight from 10 messures:\t");
-  //float floatGewicht = (scale.get_units(10) - offset);
-  //SerialUSB.println(floatGewicht);
-  //dhtPrint(); // Print serial reading of DHT reading
-  //dtostrf(floatGewicht, 6, 2, charGewicht);
-  //stringGewicht = charGewicht;  
-  //SerialCloud.println("&field1="+stringGewicht);
-  SerialCloud.println(dhtJSON()); // Send JSON DHT data to Hologram Cloud  
+  HologramCloud.sendMessage(dhtJSON()); // Send JSON DHT data to Hologram Cloud  
   scale.power_down();
+  HologramCloud.disconnect();
+  digitalWrite(D09, LOW);
   Dash.deepSleepMin(60);
-  //Dash.snooze(3530000);
-
-
+  
 }
 
-/*void dhtPrint() {
-  Serial.print("Humidity: ");
-  Serial.print(dht.readHumidity());
-  Serial.println(" %\t");
-  Serial.println("Temperature: ");
-  Serial.print(dht.readTemperature());
-  Serial.print(" *C ");
-  Serial.print(dht.readTemperature(true));
-  Serial.println(" *F\t");
-  Serial.println("SOC: ");
-  Serial.print(Dash.batteryPercentage());
-  Serial.println(" %\t");
-}
-*/
+
 String dhtJSON() {
   char buf[8];
   dtostrf((scale.get_units(10) - offset),4,2,buf);
   String ret = "&field1=";
   ret.concat(buf);
-  dtostrf(dht.readTemperature(),4,2,buf);
+  dtostrf(dht_pinR04.readTemperature(),4,2,buf);
   ret.concat("&field2=");
   ret.concat(buf);
-  //ret.concat(", ");
-  dtostrf(dht.readHumidity(),4,2,buf);
+  dtostrf(dht_pinR04.readHumidity(),4,2,buf);
   ret.concat("&field3=");
   ret.concat(buf);
-  //ret.concat(", ");
-  dtostrf(Charger.batteryPercentage(),4,2,buf);
+  dtostrf(dht_pinR05.readTemperature(),4,2,buf);
   ret.concat("&field4=");
   ret.concat(buf);
-  //ret.concat("}");
-  //Serial.println(ret);
+  //ret.concat(", ");
+  dtostrf(dht_pinR05.readHumidity(),4,2,buf);
+  ret.concat("&field5=");
+  ret.concat(buf);
+  dtostrf(Charger.batteryPercentage(),4,2,buf);
+  ret.concat("&field6=");
+  ret.concat(buf);
+ 
   return ret;
 }
 
